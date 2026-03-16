06f3687e66087459d4a15fec21ca95dd46fe1fa8
Andy Pollari <code@pollari.ch>
2026-03-16 22:59:04 +0100

    feat: wire up wifi widget

--- modules/home-manager/sketchybar_config/plugins/toggle_labels.sh
+++ modules/home-manager/sketchybar_config/plugins/toggle_labels.sh
@@ -3,6 +3,7 @@ sketchybar \
   --set battery label.drawing=toggle \
   --set volume  label.drawing=toggle \
   --set vpn     label.drawing=toggle \
+  --set wifi    label.drawing=toggle \
   --set ram     label.drawing=toggle \
   --set cpu     label.drawing=toggle \
   --set disk    label.drawing=toggle \
--- modules/home-manager/sketchybar_config/plugins/wifi.sh
+++ modules/home-manager/sketchybar_config/plugins/wifi.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-SSID=$(ipconfig getsummary en0 | grep ' SSID : ' | awk -F': ' '{print $2}')
+WIFI_IF=$(networksetup -listallhardwareports | awk '/Wi-Fi|AirPort/{found=1} found && /Device:/{print $2; exit}')
 
-IS_CONNECTED=false
-if [ -n "$SSID" ] && [ "$SSID" != "<redacted>" ] && [ "$SSID" != "You are not associated with an AirPort network." ]; then
-  IS_CONNECTED=true
-elif scutil --nwi | grep -A 3 "en0" | grep -q "Reachable"; then
-  IS_CONNECTED=true
-fi
-
-if [ "$IS_CONNECTED" = true ]; then
+if ipconfig getifaddr "$WIFI_IF" &>/dev/null; then
   ICON=󰤨
 else
   ICON=󰤮
