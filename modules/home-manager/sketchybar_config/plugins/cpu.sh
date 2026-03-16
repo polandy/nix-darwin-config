23b599518a3cb68bbf400c32c02201c60f9eebe8
Andy Pollari <code@pollari.ch>
2026-03-16 21:22:07 +0100

    feat: cpu/ram color thresholds

--- modules/home-manager/sketchybar_config/plugins/cpu.sh
+++ modules/home-manager/sketchybar_config/plugins/cpu.sh
@@ -1,1 +1,13 @@
-sketchybar -m --set $NAME label=$(ps -A -o %cpu | awk '{s+=$1} END {printf "%.0f%%\n", s}')
+#!/bin/bash
+source "$CONFIG_DIR/colors.sh"
+
+# Normalize to 0-100% across all cores so thresholds are meaningful
+CORES=$(sysctl -n hw.logicalcpu)
+CPU=$(ps -A -o %cpu | awk -v cores="$CORES" '{s+=$1} END {printf "%.0f", s/cores}')
+
+if [ "$CPU" -gt 90 ]; then   COLOR=$RED
+elif [ "$CPU" -gt 70 ]; then COLOR=$YELLOW
+else                         COLOR=$WHITE
+fi
+
+sketchybar --set $NAME label="${CPU}%" label.color=$COLOR
--- modules/home-manager/sketchybar_config/plugins/ram.sh
+++ modules/home-manager/sketchybar_config/plugins/ram.sh
@@ -1,1 +1,11 @@
-sketchybar -m --set $NAME label=$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%.0f%%\n", 100-$5) }')
+#!/bin/bash
+source "$CONFIG_DIR/colors.sh"
+
+RAM=$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%.0f", 100-$5) }')
+
+if [ "$RAM" -gt 90 ]; then   COLOR=$RED
+elif [ "$RAM" -gt 70 ]; then COLOR=$YELLOW
+else                         COLOR=$WHITE
+fi
+
+sketchybar --set $NAME label="${RAM}%" label.color=$COLOR
