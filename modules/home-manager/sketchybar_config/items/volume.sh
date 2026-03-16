4f97beb271f21f458f9788ac63cbca5f57e7ba6c
Andy Pollari <code@pollari.ch>
2026-03-16 21:07:23 +0100

    feat: control volume by scrolling on the bar icon
    Replaces the click-to-toggle popup/slider approach (slider interaction
    inside sketchybar popups doesn't fire script events) with a simpler
    mouse.scrolled subscription — scroll up/down on the volume item to
    adjust system volume in 5% increments.

--- modules/home-manager/sketchybar_config/items/volume.sh
+++ modules/home-manager/sketchybar_config/items/volume.sh
@@ -1,3 +1,4 @@
 sketchybar --add item volume right \
-           --set volume script="$PLUGIN_DIR/volume.sh" \
-           --subscribe volume volume_change 
+           --set volume \
+                 script="$PLUGIN_DIR/volume.sh" \
+           --subscribe volume volume_change mouse.scrolled
--- modules/home-manager/sketchybar_config/plugins/volume.sh
+++ modules/home-manager/sketchybar_config/plugins/volume.sh
@@ -2,22 +2,31 @@
 
 source "$CONFIG_DIR/icons.sh"
 
-if [ "$SENDER" = "volume_change" ]; then
-  VOLUME=$INFO
-
+update_display() {
+  local VOLUME=$1
+  local ICON
   case $VOLUME in
-    [6-9][0-9]|100) ICON=$VOLUME_100
-    ;;
-    [3-5][0-9]) ICON=$VOLUME_66
-    ;;
-    [1-2][0-9]) ICON=$VOLUME_33
-    ;;
-    [1-9]) ICON=$VOLUME_10
-    ;;
-    0) ICON=$VOLUME_0
-    ;;
-    *) ICON=$VOLUME_100
+    [6-9][0-9]|100) ICON=$VOLUME_100 ;;
+    [3-5][0-9])     ICON=$VOLUME_66  ;;
+    [1-2][0-9])     ICON=$VOLUME_33  ;;
+    [1-9])          ICON=$VOLUME_10  ;;
+    0)              ICON=$VOLUME_0   ;;
+    *)              ICON=$VOLUME_100 ;;
   esac
+  sketchybar --set volume icon="$ICON" label="$VOLUME%"
+}
 
-  sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
+if [ "$SENDER" = "mouse.scrolled" ]; then
+  CURRENT=$(osascript -e "output volume of (get volume settings)")
+  # SCROLL_DELTA is negative when scrolling up (increase) on macOS
+  if [ "$SCROLL_DELTA" -lt 0 ]; then
+    NEW=$(( CURRENT + 5 ))
+  else
+    NEW=$(( CURRENT - 5 ))
+  fi
+  [ $NEW -gt 100 ] && NEW=100
+  [ $NEW -lt 0 ]   && NEW=0
+  osascript -e "set volume output volume $NEW"
+elif [ "$SENDER" = "volume_change" ]; then
+  update_display "$INFO"
 fi
