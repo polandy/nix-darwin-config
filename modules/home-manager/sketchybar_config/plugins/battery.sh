56bf118858e5a154a3c922d2a5442b31af0b66ce
Andy Pollari <code@pollari.ch>
2026-03-16 15:57:48 +0100

    fix: restore battery indicator and update to SF Symbols 6

--- modules/home-manager/sketchybar_config/plugins/battery.sh
+++ modules/home-manager/sketchybar_config/plugins/battery.sh
@@ -6,27 +6,35 @@ source "$CONFIG_DIR/colors.sh"
 PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(pmset -g batt | grep 'AC Power')
 
-if [ $PERCENTAGE = "" ]; then
+if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
-DRAWING=on
 COLOR=$WHITE
 case ${PERCENTAGE} in
-  9[0-9]|100) ICON=$BATTERY_100; DRAWING=off
+  9[0-9]|100) ICON=􀛨
   ;;
-  [6-8][0-9]) ICON=$BATTERY_75; DRAWING=off
+  8[0-9]) ICON=􀺸
   ;;
-  [3-5][0-9]) ICON=$BATTERY_50
+  7[0-9]) ICON=􀺸
   ;;
-  [1-2][0-9]) ICON=$BATTERY_25; COLOR=$ORANGE
+  6[0-9]) ICON=􀺶
   ;;
-  *) ICON=$BATTERY_0; COLOR=$RED
+  5[0-9]) ICON=􀺶
+  ;;
+  4[0-9]) ICON=􀺶
+  ;;
+  3[0-9]) ICON=􀺴
+  ;;
+  2[0-9]) ICON=􀛩; COLOR=$RED
+  ;;
+  1[0-9]) ICON=􀛩; COLOR=$RED
+  ;;
+  *) ICON=􀛪; COLOR=$RED
 esac
 
 if [[ $CHARGING != "" ]]; then
-  ICON=$BATTERY_CHARGING
-  DRAWING=off
+  ICON=􀢋
 fi
 
-sketchybar --set $NAME drawing=$DRAWING icon="$ICON" icon.color=$COLOR label="${PERCENTAGE}%"
+sketchybar --set $NAME icon="$ICON" icon.color=$COLOR label="${PERCENTAGE}%"
