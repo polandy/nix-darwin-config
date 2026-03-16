1324d8ecca98065c9284862b07cf11bc963a3c05
Andy Pollari <code@pollari.ch>
2026-03-16 21:30:39 +0100

    feat: wire up network speed widget

--- modules/home-manager/sketchybar_config/plugins/network.sh
+++ modules/home-manager/sketchybar_config/plugins/network.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 
 UPDOWN=$(ifstat -i "en0" -b 0.1 1 | tail -n1)
-DOWN=$(echo $UPDOWN | awk "{ print \$1 }" | cut -f1 -d ".")
-UP=$(echo $UPDOWN | awk "{ print \$2 }" | cut -f1 -d ".")
+DOWN=$(echo $UPDOWN | awk '{ print $1 }' | cut -f1 -d ".")
+UP=$(echo $UPDOWN | awk '{ print $2 }' | cut -f1 -d ".")
 
-DOWN_FORMAT=""
-if [ "$DOWN" -gt "999" ]; then
-  DOWN_FORMAT=$(echo $DOWN | awk '{ printf "%03.0f Mbps", $1 / 1000}')
-else
-  DOWN_FORMAT=$(echo $DOWN | awk '{ printf "%03.0f kbps", $1}')
-fi
+format_speed() {
+  local val=$1
+  if [ "$val" -gt 999 ]; then
+    awk "BEGIN { printf \"%.1fM\", $val / 1000 }"
+  else
+    echo "${val}K"
+  fi
+}
 
-UP_FORMAT=""
-if [ "$UP" -gt "999" ]; then
-  UP_FORMAT=$(echo $UP | awk '{ printf "%03.0f Mbps", $1 / 1000}')
-else
-  UP_FORMAT=$(echo $UP | awk '{ printf "%03.0f kbps", $1}')
-fi
+DOWN_FMT=$(format_speed $DOWN)
+UP_FMT=$(format_speed $UP)
 
-sketchybar -m --set network_down label="$DOWN_FORMAT" icon.highlight=$(if [ "$DOWN" -gt "0" ]; then echo "on"; else echo "off"; fi) \
-                    --set network_up label="$UP_FORMAT" icon.highlight=$(if [ "$UP" -gt "0" ]; then echo "on"; else echo "off"; fi)
+sketchybar --set network label="↓${DOWN_FMT}  ↑${UP_FMT}"
