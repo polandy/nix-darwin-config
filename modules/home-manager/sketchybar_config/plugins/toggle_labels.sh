767c101196f1f38bb2e8ea35ac594dac28dec033
Andy Pollari <code@pollari.ch>
2026-03-16 21:53:44 +0100

    feat: click front_app to toggle minimal mode

---
+++ modules/home-manager/sketchybar_config/plugins/toggle_labels.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sketchybar \
+  --set battery label.drawing=toggle \
+  --set volume  label.drawing=toggle \
+  --set vpn     label.drawing=toggle \
+  --set ram     label.drawing=toggle \
+  --set cpu     label.drawing=toggle \
+  --set disk    label.drawing=toggle \
+  --set network label.drawing=toggle
