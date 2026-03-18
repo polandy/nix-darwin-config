e696c56fd35f3577a596ea0791b5b2226265bc01
Andy Pollari <code@pollari.ch>
2026-03-18 10:07:30 +0100

    feat: migrate git config to home-manager with aliases and settings

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -3,6 +3,25 @@
 {
   programs.git = {
     enable = true;
-    extraConfig.core.editor = "nvim";
+    settings = {
+      core.editor = "nvim";
+      push.autoSetupRemote = true;
+      init.defaultBranch = "master";
+      alias = {
+      co = "checkout";
+      ci = "commit";
+      cm = "commit";
+      st = "status";
+      ad = "add";
+      df = "diff";
+      dfc = "diff --cached";
+      br = "branch";
+      lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
+      lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+      subup = "submodule update --init --recursive";
+      subst = "submodule status --recursive";
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      };
+    };
   };
 }
