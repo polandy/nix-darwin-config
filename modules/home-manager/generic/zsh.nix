f3f72d9cc18c7cad08dc978be046b6a8658dc6a8
Andy Pollari <code@pollari.ch>
2026-03-27 22:10:52 +0100

    feat(zsh): add fzf-tab completion and migrate to initContent
    Add fzf and fzf-tab plugin for fuzzy tab completions with file preview
    on cd. Migrate from initExtraBeforeCompInit/initExtra to initContent
    with mkOrder to ensure correct plugin load ordering.

--- modules/home-manager/generic/zsh.nix
+++ modules/home-manager/generic/zsh.nix
@@ -5,7 +5,7 @@
   programs.zoxide.enableZshIntegration = true;
   programs.mise.enableZshIntegration = true;
 
-  programs.starship = {
+  programs.fzf = {
     enable = true;
     enableZshIntegration = true;
   };
@@ -25,6 +25,12 @@
     };
 
     plugins = [
+      # fzf-tab must be loaded after compinit and before other completion plugins
+      {
+        name = "fzf-tab";
+        src = pkgs.zsh-fzf-tab;
+        file = "share/fzf-tab/fzf-tab.plugin.zsh";
+      }
       {
         name = "zsh-autosuggestions";
         src = pkgs.zsh-autosuggestions;
@@ -55,90 +61,102 @@
       "9.." = "cd ../../../../../../../../../";
     };
 
-    initExtraBeforeCompInit = ''
-      # Homebrew (macOS only)
-      if [[ -d /opt/homebrew ]]; then
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-      fi
-    '';
-
-    initExtra = ''
-      # VI mode
-      bindkey -v
-      export KEYTIMEOUT=1
-
-      # Autosuggestion config (matches fish autosuggestion colour)
-      ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#949494'
-      ZSH_AUTOSUGGEST_STRATEGY=(history completion)
-
-      # Key bindings (matching fish config)
-      bindkey '^E' autosuggest-execute    # Ctrl+E: accept suggestion and execute
-      bindkey '^H' backward-kill-word     # Ctrl+Backspace
-      bindkey '^[[1;5D' backward-word     # Ctrl+Left
-      bindkey '^[[1;5C' forward-word      # Ctrl+Right
-
-      # macOS locale
-      function darwin_locale() {
-        export LANG=en_US.UTF-8
-        export LC_ALL=en_US.UTF-8
-        export LC_CTYPE=en_US.UTF-8
-      }
-
-      # Archive extractor (ported from fish)
-      function x() {
-        if [[ -z "$1" ]]; then
-          echo "Error: No file specified."
-          return 1
+    initContent = lib.mkMerge [
+      # Before compinit (order 550)
+      (lib.mkOrder 550 ''
+        # Homebrew (macOS only)
+        if [[ -d /opt/homebrew ]]; then
+          eval "$(/opt/homebrew/bin/brew shellenv)"
         fi
-        for f in "$@"; do
-          if [[ -e "$f" ]]; then
-            case "$f" in
-              *.tar.bz2) tar xvjf "$f" ;;
-              *.tar.gz)  tar xvzf "$f" ;;
-              *.bz2)     bunzip2 "$f" ;;
-              *.rar)     unrar x "$f" ;;
-              *.gz)      gunzip "$f" ;;
-              *.tar)     tar xvf "$f" ;;
-              *.tbz2)    tar xvjf "$f" ;;
-              *.tgz)     tar xvzf "$f" ;;
-              *.zip)     unzip "$f" ;;
-              *.Z)       uncompress "$f" ;;
-              *.7z)      7z x "$f" ;;
-              *)         echo "'$f' cannot be extracted via x" ;;
-            esac
-          else
-            echo "'$f' is not a valid file"
-          fi
-        done
-      }
+      '')
 
-      # File growth speed monitor (ported from fish)
-      function speed() {
-        while true; do
-          local old new spd size
-          old=$(du -sk "$1" | cut -f1)
-          sleep 1
-          new=$(du -sk "$1" | cut -f1)
-          spd=$(( new - old ))
-          size=$(du -sh "$1" | cut -f1)
-          echo "$spd KB/s - $size"
-        done
-      }
+      # After compinit (default order)
+      ''
+        # fzf-tab: use fzf for all tab completions
+        zstyle ':fzf-tab:*' fzf-min-height 15
+        zstyle ':fzf-tab:*' switch-group '<' '>'
+        # Show file preview when completing cd
+        zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --tree --color=always --icons $realpath 2>/dev/null || ls $realpath'
+        # Substring matching for all completions
+        zstyle ':completion:*' matcher-list ''' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
-      # Git branch cleanup (ported from fish)
-      function git_clean_branches() {
-        local base_branch=develop
-        git checkout "$base_branch"
-        git fetch -p
-        while IFS= read -r branch; do
-          if ! git branch -r | xargs -n1 basename | grep -qx "$branch"; then
-            git branch -d "$branch"
+        # VI mode
+        bindkey -v
+        export KEYTIMEOUT=1
+
+        # Autosuggestion config (matches fish autosuggestion colour)
+        ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#949494'
+        ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+
+        # Key bindings (matching fish config)
+        bindkey '^E' autosuggest-execute    # Ctrl+E: accept suggestion and execute
+        bindkey '^H' backward-kill-word     # Ctrl+Backspace
+        bindkey '^[[1;5D' backward-word     # Ctrl+Left
+        bindkey '^[[1;5C' forward-word      # Ctrl+Right
+
+        # macOS locale
+        function darwin_locale() {
+          export LANG=en_US.UTF-8
+          export LC_ALL=en_US.UTF-8
+          export LC_CTYPE=en_US.UTF-8
+        }
+
+        # Archive extractor (ported from fish)
+        function x() {
+          if [[ -z "$1" ]]; then
+            echo "Error: No file specified."
+            return 1
           fi
-        done < <(git branch --merged "$base_branch" \
-          | grep -v "\(master\|$base_branch\|\*\)" \
-          | awk '{print $1}')
-      }
-    '';
+          for f in "$@"; do
+            if [[ -e "$f" ]]; then
+              case "$f" in
+                *.tar.bz2) tar xvjf "$f" ;;
+                *.tar.gz)  tar xvzf "$f" ;;
+                *.bz2)     bunzip2 "$f" ;;
+                *.rar)     unrar x "$f" ;;
+                *.gz)      gunzip "$f" ;;
+                *.tar)     tar xvf "$f" ;;
+                *.tbz2)    tar xvjf "$f" ;;
+                *.tgz)     tar xvzf "$f" ;;
+                *.zip)     unzip "$f" ;;
+                *.Z)       uncompress "$f" ;;
+                *.7z)      7z x "$f" ;;
+                *)         echo "'$f' cannot be extracted via x" ;;
+              esac
+            else
+              echo "'$f' is not a valid file"
+            fi
+          done
+        }
+
+        # File growth speed monitor (ported from fish)
+        function speed() {
+          while true; do
+            local old new spd size
+            old=$(du -sk "$1" | cut -f1)
+            sleep 1
+            new=$(du -sk "$1" | cut -f1)
+            spd=$(( new - old ))
+            size=$(du -sh "$1" | cut -f1)
+            echo "$spd KB/s - $size"
+          done
+        }
+
+        # Git branch cleanup (ported from fish)
+        function git_clean_branches() {
+          local base_branch=develop
+          git checkout "$base_branch"
+          git fetch -p
+          while IFS= read -r branch; do
+            if ! git branch -r | xargs -n1 basename | grep -qx "$branch"; then
+              git branch -d "$branch"
+            fi
+          done < <(git branch --merged "$base_branch" \
+            | grep -v "\(master\|$base_branch\|\*\)" \
+            | awk '{print $1}')
+        }
+      ''
+    ];
   };
 
   # Abbreviations for zsh-abbr (same as fish shellAbbrs).
