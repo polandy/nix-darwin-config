eb3fd7a239b5514e60b58d45e1ef4d3c596da284
Andy Pollari <andy.pollari@sbb.ch>
2026-03-28 22:16:39 +0100

    fix(x1): add sops key file and allow unfree packages
    Fixes home-manager switch failing due to missing sops age key
    configuration and zsh-abbr's cc-by-nc-sa-40 license being blocked.

--- hosts/x1/home.nix
+++ hosts/x1/home.nix
@@ -1,4 +1,6 @@
 { config, pkgs, ... }: {
+  nixpkgs.config.allowUnfree = true;
+
   imports = [
     ../../modules/home-manager/generic
     ../../modules/home-manager/generic/ssh.nix
@@ -8,6 +10,8 @@
   home.stateVersion = "24.05";
   home.username = "andy";
   home.homeDirectory = "/home/andy";
+
+  sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 
   home.sessionVariables = {
     SOPS_AGE_KEY_FILE = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
